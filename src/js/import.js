60d7eb0a8cecfd09b17d466fc0ed76d3196d08d1
Corbinhol <84656556+Corbinhol@users.noreply.github.com>
2023-05-16 22:42:57 -0500

    Finished HTML for small card

--- src/js/import.js
+++ src/js/import.js
@@ -2,5 +2,7 @@ import '../css/modern-normalize.css';
 import '../css/header.css';
 import '../css/toggle.css'
 import '../css/utility.css';
+import '../css/main-card.css'
+import '../css/overview.css'
 
 import '../js/light_mode.js';
